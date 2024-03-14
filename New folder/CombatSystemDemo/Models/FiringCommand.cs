e4500315abfb198a64616d877ba1fcde3ac69a68
Amiel Cohen <amielc1@gmail.com>
2024-03-14 16:18:08 +0200

    Remove combatProject

---
+++ New folder/CombatSystemDemo/Models/FiringCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatSystem.Models
+{
+    public class FiringCommand
+    {
+        public int TargetId;
+        public int WeaponType;
+    }
+}
---
+++ New folder/CombatSystemDemo/Models/FiringStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatSystem.Models
+{
+    public class FiringStatus
+    {
+        public int TargetId;
+        public bool Success;
+    }
+}
