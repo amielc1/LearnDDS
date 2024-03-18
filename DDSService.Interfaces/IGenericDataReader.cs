6214c3742d34e7e49e30c04867433018ea169762
Amiel Cohen <amielc1@gmail.com>
2024-03-18 10:59:13 +0200

    Remove old projects

--- DDSService.Interfaces/IGenericDataReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenDDSharp.DDS;
-
-namespace DDSService.Interface
-{
-    public interface IGenericDataReader<T>
-    {
-        ReturnCode Take(List<T> dataValues, List<SampleInfo> sampleInfos);
-    }
-}
--- New folder/CombatSystemDemo/Configuration/DdsConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CombatSystem.Configuration;
-
-public class DdsConfiguration
-{
-    public int DomainId { get; set; } = 42;
-    public string Topic { get; set; } = "MissionTopic";
-    public string DCPSConfigFile { get; set; } = "-DCPSConfigFile";
-    public string rtps { get; set; } = "rtps.ini";
-}
